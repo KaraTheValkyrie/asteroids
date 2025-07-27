c2167776a9e182c23581ca7a0beaf5c6493f4772
MercurialKara <videogmer314@yahoo.com>
2025-07-27 14:39:30 -0500

    added time delta functionality

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #initialize things and set the screen size
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #set some variables to control the game's FPS
+    clock = pygame.time.Clock()
+    dt = 0
+
+
+
     while True:
-        # handle exiting the game
+        #handle exiting the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -24,6 +31,11 @@ def main():
         #render a black screen
         screen.fill(000000)
         pygame.display.flip()
+
+        #tell the game to wait 1/60 of a second
+        dt = clock.tick(60) / 1000
+
+
 
 
 if __name__ == "__main__":
